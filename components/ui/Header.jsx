c9d9bc74e4ca136ca5f29c979ab6a0dbe5a69830
CodeVanguard <65644463+DeepakChowdary09@users.noreply.github.com>
2026-06-06 23:44:31 +0530

    Update Header.jsx

--- components/ui/Header.jsx
+++ components/ui/Header.jsx
@@ -47,7 +47,7 @@ const GrowthToolsDropdown = () => (
       <DropdownMenuItem>
         <Link href="/resume" className="flex items-center gap-2">
           <FileText className="h-4 w-4 mr-2" />
-          <span>Build Resume</span>
+          <span>Bu Resume</span>
         </Link>
       </DropdownMenuItem>
       <DropdownMenuItem>
